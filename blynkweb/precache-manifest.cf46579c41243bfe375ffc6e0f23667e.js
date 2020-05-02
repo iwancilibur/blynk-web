829d2d1530f1854de3bb86d4c8011da9b9fdc03b
Iwan Cilibur <32497273+iwancilibur@users.noreply.github.com>
2020-05-02 20:08:30 +0700

    Add files via upload

---
+++ blynkweb/precache-manifest.cf46579c41243bfe375ffc6e0f23667e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7f1279124c0ce54e94f2",
+    "url": "./static/js/main.7f127912.chunk.js"
+  },
+  {
+    "revision": "3cfef3a38dcbfc664591",
+    "url": "./static/js/1.3cfef3a3.chunk.js"
+  },
+  {
+    "revision": "7f1279124c0ce54e94f2",
+    "url": "./static/css/main.37edd922.chunk.css"
+  },
+  {
+    "revision": "3cfef3a38dcbfc664591",
+    "url": "./static/css/1.af09f1af.chunk.css"
+  },
+  {
+    "revision": "970d1f511271254de0ef283876c55eea",
+    "url": "./index.html"
+  }
+];
